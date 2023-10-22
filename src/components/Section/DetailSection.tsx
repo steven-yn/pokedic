c6cf1e13ec220dadcf098ab806d16cab31a04977
steven-yn <ssungyeon94@gmail.com>
2023-10-22 23:02:24 +0900

    refactor: detail section 분리

---
+++ src/components/Section/DetailSection.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { inter } from '@/pages/_app';
+import usePokemonDetailData from '../PokemonInfo/hooks/usePokemonDetailData';
+import PokemonInfo from '../PokemonInfo/PokemonInfo';
+
+const DetailSection = () => {
+  const { data } = usePokemonDetailData();
+
+  if (!data || !data.responseData.koNames) return null;
+
+  return (
+    <PokemonInfo className={inter.className}>
+      <PokemonInfo.Title>
+        {data.responseData.id}.&nbsp;
+        {data.responseData.koNames[0].name || data.responseData.name || ''}
+      </PokemonInfo.Title>
+      <PokemonInfo.Profile sprites={data.responseData.sprites} />
+      <PokemonInfo.Paragraph>
+        무게 : {data.responseData.weight}hg
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        키 : {data.responseData.height}cm
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        타입 :&nbsp;
+        {data.responseData.types.map((type) => type.type.name).join(', ')}
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        특성 :&nbsp;
+        {data.responseData.abilities
+          .map((ability) => ability.ability.name)
+          .join(', ')}
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        스텟 :&nbsp;
+        {data.responseData.stats
+          .map((stat) => `${stat.stat.name}(${stat.base_stat})`)
+          .join(', ')}
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        획득 가능 경험치 :&nbsp;
+        {data.responseData.base_experience}
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        기술 :&nbsp;
+        {data.responseData.moves.map((move) => move.move.name).join(', ')}
+      </PokemonInfo.Paragraph>
+      <PokemonInfo.Paragraph>
+        등장 시리즈 (포켓몬 게임) :&nbsp;
+        {data.responseData.game_indices
+          .map((game) => game.version.name)
+          .join(', ')}
+      </PokemonInfo.Paragraph>
+      {data.responseData.evolutionChain &&
+        data.responseData.evolutionChain.evolves_to.length > 0 && (
+          <PokemonInfo.Paragraph>
+            진화 단계 :&nbsp;
+            <PokemonInfo.Evolution
+              key={data.responseData.evolutionChain.species.name}
+              chainLink={data.responseData.evolutionChain}
+            />
+          </PokemonInfo.Paragraph>
+        )}
+    </PokemonInfo>
+  );
+};
+
+export default DetailSection;
--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -2,11 +2,10 @@ import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import React from 'react';
 import usePokemonDetailData from '@/components/PokemonInfo/hooks/usePokemonDetailData';
-import PokemonInfo from '@/components/PokemonInfo/PokemonInfo';
+import DetailSection from '@/components/Section/DetailSection';
 import { pokemonKeys } from '@/const/queries';
 import FetchPokemon from '@/services/FetchPokemon';
 import commonServerSiderProps from '@/utils/commonServerSiderProps';
-import { inter } from './_app';
 
 const PokemonDetail = () => {
   const { data } = usePokemonDetailData();
@@ -22,59 +21,7 @@ const PokemonDetail = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PokemonInfo className={inter.className}>
-        <PokemonInfo.Title>
-          {data.responseData.id}.&nbsp;
-          {data.responseData.koNames[0].name || data.responseData.name || ''}
-        </PokemonInfo.Title>
-        <PokemonInfo.Profile sprites={data.responseData.sprites} />
-        <PokemonInfo.Paragraph>
-          무게 : {data.responseData.weight}hg
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          키 : {data.responseData.height}cm
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          타입 :&nbsp;
-          {data.responseData.types.map((type) => type.type.name).join(', ')}
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          특성 :&nbsp;
-          {data.responseData.abilities
-            .map((ability) => ability.ability.name)
-            .join(', ')}
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          스텟 :&nbsp;
-          {data.responseData.stats
-            .map((stat) => `${stat.stat.name}(${stat.base_stat})`)
-            .join(', ')}
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          획득 가능 경험치 :&nbsp;
-          {data.responseData.base_experience}
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          기술 :&nbsp;
-          {data.responseData.moves.map((move) => move.move.name).join(', ')}
-        </PokemonInfo.Paragraph>
-        <PokemonInfo.Paragraph>
-          등장 시리즈 (포켓몬 게임) :&nbsp;
-          {data.responseData.game_indices
-            .map((game) => game.version.name)
-            .join(', ')}
-        </PokemonInfo.Paragraph>
-        {data.responseData.evolutionChain &&
-          data.responseData.evolutionChain.evolves_to.length > 0 && (
-            <PokemonInfo.Paragraph>
-              진화 단계 :&nbsp;
-              <PokemonInfo.Evolution
-                key={data.responseData.evolutionChain.species.name}
-                chainLink={data.responseData.evolutionChain}
-              />
-            </PokemonInfo.Paragraph>
-          )}
-      </PokemonInfo>
+      <DetailSection />
     </>
   );
 };
